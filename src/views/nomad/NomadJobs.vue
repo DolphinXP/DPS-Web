ef6aaf9939f03230f645b540e8f315d473301c61
DolphinXP <aceglgm@gmail.com>
2024-03-04 16:35:16 +0800

    add task log view

--- src/views/nomad/NomadJobs.vue
+++ src/views/nomad/NomadJobs.vue
@@ -17,58 +17,77 @@
         <a @click="handleStop(record)">Stop</a>
         <a-divider type="vertical"/>
         <a @click="handleDel(record)">Delete</a>
+        <a-divider type="vertical"/>
+        <a @click="handleLog(record)">StdoutLog</a>
       </template>
     </a-table>
   </div>
 
+  <a-modal v-model:open="openLog" footer="" title="Log view" width="80%">
+    <pre class="log-view" v-text="logContent"></pre>
+  </a-modal>
 </template>
 
 <script lang="ts" setup>
-import {onMounted, onUnmounted, ref} from "vue";
+import {onMounted, onUnmounted, ref, watch} from "vue";
 import axios from "axios";
 
 const dataSource = ref([]);
 const refreshInterval = ref('1');
-let timeoutId = null;
+const openLog = ref(false);
+const logContent = ref('');
+
+let currentId = ref('');
+let refreshTimer: ReturnType<typeof setInterval> | null = null;
+let logTimer: ReturnType<typeof setInterval> | null = null;
 
 onMounted(() => {
   updateData();
+
+  watch(openLog, (newValue, oldValue) => {
+    if (newValue) {
+      // Enable logTimer
+      logTimer = setInterval(() => {
+        axios.get('http://localhost:8080/api/v1/nomad/logs/' + currentId.value + "/stdout")
+            .then(response => {
+              console.log('response', response);
+              logContent.value = response.data.data;
+            }).catch(error => {
+          console.log(error);
+          logContent.value = error.response.data.data;
+        })
+      }, 300);
+    } else {
+      // Disable logTimer
+      if (logTimer) {
+        clearInterval(logTimer);
+        logTimer = null;
+      }
+    }
+  });
 });
 
 const updateData = () => {
   axios.get('http://localhost:8080/api/v1/nomad/jobs').then(
       response => {
         dataSource.value = response.data.data;
-        console.log(dataSource.value)
-        // map task status to new field: TaskStatus
-        // dataSource.value = response.data.data.map(item => {
-        //   const statusKeys = ['Queued', 'Complete', 'Failed', 'Running', 'Starting', 'Lost', 'Unknown'];
-        //   let taskStatus = '';
-        //   for (let key of statusKeys) {
-        //     if (item.JobSummary.Summary.group1[key] === 1) {
-        //       taskStatus = key;
-        //       break;
-        //     }
-        //   }
-        //   return {...item, TaskStatus: taskStatus};
-        // });
-
+        dataSource.value.sort((a, b) => a.SubmitTime - b.SubmitTime);
       }
   ).catch(error => console.log(error))
       .finally(() => {
         // Clear the previous timeout if it exists
-        if (timeoutId) {
-          clearTimeout(timeoutId);
+        if (refreshTimer) {
+          clearTimeout(refreshTimer);
         }
         // Schedule the next update
-        timeoutId = setTimeout(updateData, refreshInterval.value * 1000);
+        refreshTimer = setTimeout(updateData, refreshInterval.value * 1000);
       });
 };
 
 onUnmounted(() => {
   // Clear the timeout when the component is unmounted
-  if (timeoutId) {
-    clearTimeout(timeoutId);
+  if (refreshTimer) {
+    clearTimeout(refreshTimer);
   }
 });
 
@@ -116,7 +135,7 @@ const handleNew = (id: string) => {
 }
 
 const handleStop = (record: any) => {
-  let id = record.ID;
+  currentId.value = record.ID;
   axios.delete('http://localhost:8080/api/v1/nomad/jobs/stop/' + id)
       .then(response => {
         console.log('response', response);
@@ -125,7 +144,7 @@ const handleStop = (record: any) => {
 };
 
 const handleDel = (record: any) => {
-  let id = record.ID;
+  currentId.value = record.ID;
   axios.delete('http://localhost:8080/api/v1/nomad/jobs/delete/' + id)
       .then(response => {
         console.log('response', response);
@@ -133,11 +152,25 @@ const handleDel = (record: any) => {
       })
 };
 
+const handleLog = (record: any) => {
+  currentId.value = record.ID;
+  logContent.value = '';
+  openLog.value = true;
+};
+
+
 </script>
 
 <style scoped>
 .pull-right {
   margin-bottom: 8px;
   float: right;
+}
+
+.log-view {
+  height: 80vh;
+  overflow: auto;
+  background-color: #2c3e50;
+  color: #ecf0f1;
 }
 </style>
